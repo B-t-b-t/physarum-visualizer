99473264c0f5763a8246e28251e8f5a714c33d30
Bastian Herentrey <bastian-herentrey@t-online.de>
2026-02-26 11:01:14 +0100

    Added Build Files

---
+++ mingw-w64-toolchain.cmake
@@ -0,0 +1,22 @@
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# use mingw-w64 compilers instead of Linux gcc/g++
+SET(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
+
+# set paths for MingW32 on Linux and used libraries
+SET(CMAKE_FIND_ROOT_PATH
+    /usr/x86_64-w64-mingw32
+    ${CMAKE_SOURCE_DIR}/../libraries
+)
+
+# search headers and libraries in the target environment
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# use static linking
+SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
